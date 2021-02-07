8fe49fe6e5911f3559ecae448d841a7ae0e4a049
Jake Mcneill <jake-cryptic@users.noreply.github.com>
2021-02-07 18:11:20 +0000

    Added UK filters and tests

--- src/CellFilters/countries/united_kingdom.py
+++ src/CellFilters/countries/united_kingdom.py
@@ -1,5 +1,4 @@
 class Filter:
-
 	mnc_filters = {}
 
 	def __repr__(self):
@@ -18,13 +17,78 @@ class Filter:
 		return self.mnc_filters[mnc](enb, sid)
 
 	def o2(self, enb, sid):
-		return True
+		valid_vf_host = (
+			110, 120, 130, 140, 150, 160,  	# L08
+			112, 122, 132, 142, 152, 162,  	# L09
+			114, 124, 134, 144, 154, 164,  	# L21
+			115, 125, 135, 145, 155, 165,  	# L23-C1
+			116, 126, 136, 146, 156, 166,  	# L18
+			117, 127, 137, 147, 157, 167   	# L23-C2
+		)
+		valid_o2_host = (
+			110, 120, 130, 140, 150, 160,  # L08
+			112, 122, 132, 142, 152, 162,  # L09
+			114, 124, 134, 144, 154, 164,  # L18
+			115, 125, 135, 145, 155, 165,  # L21
+			116, 126, 136, 146, 156, 166,  # L23-C1
+			117, 127, 137, 147, 157, 167  # L23-C2
+			# 118, 128, 138	# L26 TDD?
+		)
+
+		if (3 < enb < 15000 or 100000 < enb < 115000) and sid in valid_vf_host:
+			return True
+
+		if 500000 < enb < 550000 and sid in valid_o2_host:
+			return True
+
+		return False
 
 	def vodafone(self, enb, sid):
-		return True
+		valid_vf_host = (
+			10, 20, 30, 40, 50, 60,		# L08
+			21,  # Exception for DAS
+			12, 22, 32, 42, 52, 62,		# L09
+			14, 24, 34, 44, 54, 64,		# L21
+			16, 26, 36, 46, 56, 66,		# L18
+			18, 28, 38, 48, 58, 68,		# L26
+			19, 29, 39					# L26 TDD
+		)
+		valid_o2_host = (
+			10, 20, 30, 40, 50, 60,		# L08
+			12, 22, 32, 42, 52, 62,		# L09
+			14, 24, 34, 44, 54, 64,		# L18
+			15, 25, 35, 45, 55, 65,		# L21
+			18, 28, 38, 48, 58, 68,		# L26
+		)
+
+		if 3 < enb < 15000 and sid in valid_vf_host:
+			return True
+
+		if 500000 < enb < 550000 and sid in valid_o2_host:
+			return True
+
+		return False
 
 	def three(self, enb, sid):
+		if 50000 < enb < 50050:
+			if sid == 16:
+				return True
+			return False
+
+		if 16000 < enb < 49000:
+			return False
+
+		if sid not in (0, 1, 2, 3, 4, 5, 6, 7, 8, 71, 72, 73, 74, 75, 76):
+			return False
+
 		return True
 
 	def ee(self, enb, sid):
-		return True
+		if 10000 < enb < 40000:
+			if sid > 20:
+				if sid not in (21, 24):
+					return False
+
+			return True
+
+		return False
--- src/test_cell_filter.py
+++ src/test_cell_filter.py
@@ -4,45 +4,62 @@ cf = CellFilter()
 
 tests = [
 	# Check out of bounds
-	'cf.validate("234", "10", "0", "0") == False',
-	'cf.validate("234", "15", "0", "0") == False',
-	'cf.validate("234", "20", "0", "0") == False',
-	'cf.validate("234", "30", "0", "0") == False',
+	'cf.validate("234", "10", 0, 0) == False',
+	'cf.validate("234", "15", 0, 0) == False',
+	'cf.validate("234", "20", 0, 0) == False',
+	'cf.validate("234", "30", 0, 0) == False',
 
-	'cf.validate("234", "10", "0", "256") == False',
-	'cf.validate("234", "15", "0", "256") == False',
-	'cf.validate("234", "20", "0", "256") == False',
-	'cf.validate("234", "30", "0", "256") == False',
+	'cf.validate("234", "10", 0, 256) == False',
+	'cf.validate("234", "15", 0, 256) == False',
+	'cf.validate("234", "20", 0, 256) == False',
+	'cf.validate("234", "30", 0, 256) == False',
 
 	# O2
-	'cf.validate("234", "10", "91", "110") == True',
-	'cf.validate("234", "10", "510005", "110") == True',
-	'cf.validate("234", "10", "91", "10") == False',
-	'cf.validate("234", "10", "510005", "10") == False',
+	'cf.validate("234", "10", 91, 110) == True',
+	'cf.validate("234", "10", 510005, 110) == True',
+	'cf.validate("234", "10", 112758, 116) == True',
+	'cf.validate("234", "10", 112758, 117) == True',
+	'cf.validate("234", "10", 112958, 115) == True',
+	'cf.validate("234", "10", 91, 10) == False',
+	'cf.validate("234", "10", 510000, 10) == False',
 
 	# VF
-	'cf.validate("234", "15", "91", "10") == True',
-	'cf.validate("234", "15", "510005", "10") == True',
-	'cf.validate("234", "15", "91", "110") == False',
-	'cf.validate("234", "15", "510005", "110") == False',
+	'cf.validate("234", "15", 91, 10) == True',
+	'cf.validate("234", "15", 510005, 10) == True',
+	'cf.validate("234", "15", 12958, 10) == True',
+	'cf.validate("234", "15", 12958, 14) == True',
+	'cf.validate("234", "15", 13153, 18) == True',
+	'cf.validate("234", "15", 91, 110) == False',
+	'cf.validate("234", "15", 510005, 110) == False',
+
+	# 3
+	'cf.validate("234", "20", 17000, 2) == False',
+	'cf.validate("234", "20", 40000, 2) == False',
+	'cf.validate("234", "20", 49501, 2) == True',
+	'cf.validate("234", "20", 49565, 8) == True',
+	'cf.validate("234", "20", 49522, 71) == True',
 
 	# Check Three's small cells
-	'cf.validate("234", "20", "50001", "16") == True',
-	'cf.validate("234", "20", "50001", "0") == False',
+	'cf.validate("234", "20", 50001, 16) == True',
+	'cf.validate("234", "20", 50001, 0) == False',
 
 	# EE
-	'cf.validate("234", "30", "11000", "30") == False',
-	'cf.validate("234", "30", "15350", "2") == True'
+	'cf.validate("234", "30", 11000, 30) == False',
+	'cf.validate("234", "30", 27980, 22) == False',
+	'cf.validate("234", "30", 40000, 20) == False',
+	'cf.validate("234", "30", 15350, 2) == True',
+	'cf.validate("234", "30", 15350, 5) == True',
+	'cf.validate("234", "30", 14160, 16) == True'
 ]
 
 failures = 0
 for test in tests:
+	print("\n>> " + test)
 	result = eval(test)
 
 	if result == False:
 		failures += 1
 
-	print("\n>> " + test)
 	print(result)
 
 print("\n%s tests completed, %s failed." % (len(tests), failures))
